fc144bd4839b98b0a4114bd8568cdbfb79807a3f
Michael Spagnolo <mspagnolo27@gmail.com>
2022-05-11 22:06:48 -0400

    inital commit

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+const port = process.env.PORT || 3000;
+
+const publicPath = path.join(__dirname,'..','build')
+app.use(express.static(publicPath))
+
+// app.get('/', (req, res) => {
+//   res.json({"name":"Mike","birthday":"4/27/1987"})
+// })
+
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(publicPath,'index.html'))
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
